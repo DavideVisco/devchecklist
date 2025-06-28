10bbe20fcb8e2a56cb50dcbf9ca24d4a08851191
d.viscogliosi <d.viscogliosi@reply.it>
2025-06-28 15:40:39 +0200

    Main initial commit

---
+++ src/main/java/org/davideviscogliosi/devchecklist/DevChecklistApplication.java
@@ -0,0 +1,12 @@
+package org.davideviscogliosi.devchecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevChecklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevChecklistApplication.class, args);
+    }
+}
